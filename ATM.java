49a07d83375fe81c03e72124859623c79c5920fc
NasirHerawi <nasir.herawi@gmail.com>
2021-11-07 15:43:07 +0100

    first commit

---
+++ ATM.java
@@ -0,0 +1,16 @@
+package packageATM;
+
+	import java.io.IOException;
+
+	public class ATM extends OptionMenu {
+
+		public static void main(String[] args) throws IOException {
+			OptionMenu optionMenu = new OptionMenu();
+
+			optionMenu.getLogin();
+		}
+
+	};
+
+
+
