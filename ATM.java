ca032c6bfc262faaa8a61439b7f59ffc1d83e271
NasirHerawi <nasir.herawi@gmail.com>
2021-11-07 15:49:33 +0100

    a new description was added

--- ATM.java
+++ ATM.java
@@ -5,6 +5,7 @@ package packageATM;
 	public class ATM extends OptionMenu {
 
 		public static void main(String[] args) throws IOException {
+			System.out.println("THIS IS THE MAIN CLASS OF THE PROJECT!");
 			OptionMenu optionMenu = new OptionMenu();
 
 			optionMenu.getLogin();
